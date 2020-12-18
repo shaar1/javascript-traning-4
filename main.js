7c3264471938f59635cfbbc0e7a48fac7e8d0fa3
Abdallah <abdallahchaar90@gmail.comh>
2020-12-18 11:25:23 +0100

    m

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 //simple function
 
 document.getElementById("test").innerHTML = "Hello JavaScript from Js file";
+
+//condition
+
+if (x + y === 40){
+    console.log("Good");
+} else {
+    console.log("bad");
+}
