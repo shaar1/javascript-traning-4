6d671563a4b080ed9328da9729d14d967b875f63
Abdallah <abdallahchaar90@gmail.comh>
2021-01-01 14:53:51 +0100

    träning

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+/*global consolr*/
+// Some vers for bla bla bla
+// single line comment
+/* 
+Multi line Comment Line 1 
+Multi line Comment Line 2
+*/
+/*
+*** Version 1.0
+*** This Is Test Comment
+*** this Is Line 2
+*** This Is Last Line
+*/
+
+
+
+
 var // some vers
 x = 10,
 y = 20,
@@ -12,7 +29,7 @@ theLastName = "Sayed";
 document.getElementById("test").innerHTML = "Hello JavaScript from Js file";
 
 //condition
-
+ 
 if (x + y === 40){
     console.log("Good");
 } else {
